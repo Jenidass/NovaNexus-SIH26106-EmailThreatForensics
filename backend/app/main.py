67f48251d3d6b7bda9a9f3361d95c431d51dfdbd
Jenidass <jenidass1007@gmail.com>
2026-09-25 14:38:00 +0530

    Allow deployed frontend in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
     "http://localhost:5174",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
+      "https://novanexus-sih26106-emailthreatforensics.onrender.com",
 ],
     allow_credentials=True,
     allow_methods=["*"],
